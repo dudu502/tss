248f49a32b1c70563e73afafc9acfa3cee960bad
Haijie HJ2 Pan <panhj2@lenovo.com>
2021-12-31 16:56:13 +0800

    Update Program.cs

--- BT/Program.cs
+++ BT/Program.cs
@@ -12,23 +12,15 @@ namespace Task.Switch.Structure.BT
             BehaviourTree tree = new BehaviourTree();
 
             tree.Repeat()
-                        .Sequence()
+                         .Sequence()
                                     .Do()
                                         .Start(() => Console.WriteLine("Start Hungry?"))
-                                        .Update(() =>
-                                        {
-                                            Console.WriteLine("Update Hungry?");
-                    
-                                        })
+                                        .Update(() => Console.WriteLine("Update Hungry?"))
                                         .Stop(() => Console.WriteLine("Stop Hungry?"))
                                         .End()
                                     .Do()
                                         .Start(() => Console.WriteLine("Yes,Start Very Hungry"))
-                                        .Update(() =>
-                                        {
-                                            Console.WriteLine("Yes,Update Very Hungry");
-                                   
-                                        })
+                                        .Update(() => Console.WriteLine("Yes,Update Very Hungry"))
                                         .Stop(() => Console.WriteLine("Yes,Stop Very Hungry"))
                                         .End()
                                     .Select()
@@ -36,21 +28,13 @@ namespace Task.Switch.Structure.BT
                                             .End()
                                         .Do()
                                             .Start(() => Console.WriteLine("Start Eat apple"))
-                                            .Update(() =>
-                                            {
-                                                Console.WriteLine("Update Eat apple");
-                                     
-                                            })
+                                            .Update(() => Console.WriteLine("Update Eat apple"))
                                             .GetResult(() => Node.NodeResult.Failure)
                                             .Stop(() => Console.WriteLine("Stop Eat apple"))
                                             .End()
                                         .Do()
                                             .Start(() => Console.WriteLine("Start Eat banana"))
-                                            .Update(() =>
-                                            {
-                                                Console.WriteLine("Update Eat banana");
-                           
-                                            })
+                                            .Update(() => Console.WriteLine("Update Eat banana"))
                                             .Stop(() => Console.WriteLine("Stop Eat banana"))
                                             .End();
 
